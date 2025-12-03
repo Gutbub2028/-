808ab3778b13107c2126c9b8c4a73ae74fba5a24
Gutbub2028 <101802448+Gutbub2028@users.noreply.github.com>
2025-12-03 15:24:23 +0000

    fix

---
+++ index.js
@@ -0,0 +1,12 @@
+let count =0
+function increment(){
+    count=count+1
+    document.getElementById("count-el").textContent=count
+}
+function save() {
+    let countstr = count
+    document.getElementById("l").textContent=countstr
+    document.getElementById("count-el").textContent= 0
+}
+
+
